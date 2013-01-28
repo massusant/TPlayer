ca1fd8371805cfa994fad35b85dcf180b08bdae4
snowd <xuelongwen@163.com>
2013-01-28 11:40:43 +0800

    Update src/com/heavenly/ticket/model/Passenger.java

--- src/com/heavenly/ticket/model/Passenger.java
+++ src/com/heavenly/ticket/model/Passenger.java
@@ -2,22 +2,6 @@ package com.heavenly.ticket.model;
 
 public class Passenger {
 	
-	/* {"first_letter":"SNOWDX",
-	 * "isUserSelf":"",
-	 * "mobile_no":"13646815023",
-	 * "old_passenger_id_no":"",
-	 * "old_passenger_id_type_code":"",
-	 * "old_passenger_name":"",
-	 * "passenger_flag":"0",
-	 * "passenger_id_no":"610481198805240037",
-	 * "passenger_id_type_code":"1",
-	 * "passenger_id_type_name":"",
-	 * "passenger_name":"文雪龙",
-	 * "passenger_type":"1",
-	 * "passenger_type_name":"",
-	 * "recordCount":"3"} 
-	 */
-
 	private TicketType ticketType = TicketType.ADULT;
 	private Seat seatType;
 	private IdCardType idcardType = IdCardType.GEN_IDCARD_2;
